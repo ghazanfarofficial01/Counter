9dd680d40cac71c1818e9c8d7f204d341a75924f
ghazanfarofficial01 <56466981+ghazanfarofficial01@users.noreply.github.com>
2022-07-21 21:25:19 +0530

    Add files via upload

---
+++ COUNTER/script.js
@@ -0,0 +1,19 @@
+let counts = document.querySelectorAll(".count");
+
+counts.forEach((count)=>{
+    const updateCount = ()=>{
+
+        let currentCount = parseInt(count.innerText);
+        let targetCount = count.getAttribute("data-target");
+        let increaseSpeed = 50;
+        let increaseCount = parseInt(targetCount / increaseSpeed);
+             
+          if(currentCount < targetCount) {
+              count.innerText = currentCount + increaseCount;
+          } else {
+              count.innerText = targetCount;
+          }
+          setTimeout(updateCount, increaseSpeed);
+    }
+    updateCount();
+});
